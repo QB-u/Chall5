512363990b07795fbca81de1b26bc13a6db7324d
QB-u <85237932+QB-u@users.noreply.github.com>
2022-08-29 11:49:49 +0700

    Update same.php

--- same.php
+++ same.php
@@ -2,21 +2,21 @@
     setcookie('cookie1', '1',[
         'expires' => time() + 3600,
         'path' => '/',
-        'secure' => false,
+        'secure' => true,
         'httponly' => false,
         'samesite' => 'None'
     ]);
     setcookie('cookie2','2',[
         'expires' => time() + 3600,
         'path' => '/',
-        'secure' => false,
+        'secure' => true,
         'httponly' => false,
         'samesite' => 'strict'
     ]);
     setcookie('cookie3','3',[
         'expires' => time() + 3600,
         'path' => '/',
-        'secure' => false,
+        'secure' => true,
         'httponly' => false,
         'samesite' => 'lax'
     ]);
